f68357ec94dcb0be23919c67a56b3f6cd4841f7e
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-01-20 04:38:24 -0200

    adjusting spec names

--- spec/controllers/moves_controller_spec.rb
+++ spec/controllers/moves_controller_spec.rb
@@ -1,10 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe MovesController, type: :controller do
+<<<<<<< Updated upstream
   describe "GET 'index'" do
     it "using JSON format should return a 200 response" do
       get :index, format: :json
       expect(response).to be_success
+=======
+    describe "GET 'index'" do
+        it "should return a 200 response when asking JSON format" do
+            get :index, format: :json
+            expect(response).to be_success
+        end
+        it "should return all moves when asking JSON format" do
+            FactoryGirl.create_list(:move, 5)
+            get :index, format: :json
+            parsed_response = JSON.parse(response.body)
+            expect(parsed_response.length).to eq(5)
+        end
+        it "should return a 200 response when asking XML format" do
+            get :index, format: :xml
+            expect(response).to be_success
+        end
+        it "should return all moves when asking XML format" do
+            FactoryGirl.create_list(:move, 5)
+            get :index, format: :xml
+            parsed_response = XmlSimple.xml_in(response.body)
+            expect(parsed_response['move'].length).to eq(5)
+        end
+>>>>>>> Stashed changes
     end
 
     it "using JSON format should return all types" do
--- spec/controllers/types_controller_spec.rb
+++ spec/controllers/types_controller_spec.rb
@@ -2,10 +2,34 @@ require 'rails_helper'
 require 'xmlsimple'
 
 RSpec.describe TypesController, type: :controller do
+<<<<<<< Updated upstream
   describe "GET 'index'" do
     it "using JSON format should return a 200 response" do
       get :index, format: :json
       expect(response).to be_success
+=======
+    describe "GET 'index'" do
+        it "should return a 200 response when asking JSON format" do
+            get :index, format: :json
+            expect(response).to be_success
+        end
+        it "should return all types when asking JSON format" do
+            FactoryGirl.create_list(:type, 5)
+            get :index, format: :json
+            parsed_response = JSON.parse(response.body)
+            expect(parsed_response.length).to eq(5)
+        end
+        it "should return a 200 response when asking XML format" do
+            get :index, format: :xml
+            expect(response).to be_success
+        end
+        it "should return all types when asking XML format" do
+            FactoryGirl.create_list(:type, 5)
+            get :index, format: :xml
+            parsed_response = XmlSimple.xml_in(response.body)
+            expect(parsed_response['type'].length).to eq(5)
+        end
+>>>>>>> Stashed changes
     end
 
     it "using JSON format should return all types" do
