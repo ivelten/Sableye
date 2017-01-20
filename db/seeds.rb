aea50f8216bf56b593acc4e6a38a28138a1ab6d6
Andre Brandemburg <Andre Brandemburg>
2017-01-20 12:54:01 -0200

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -591,20 +591,20 @@ avalanche = Move.create!(
 
 # B letter moves --------------------------------------------------------------------------------------------
 
-baby_doll_eyes = Move.create!(
-    name: 'baby_doll_eyes',
-    description: 'Baby-Doll Eyes',
-    type: fairy,
-    category: other,
-    power_points: 30,
-    base_power: 0,
-    accuracy: 100,
-    battle_effect: "The user stares at the target with its baby-doll eyes, which lowers its Attack stat. This move always goes first.",
-    secondary_effect:"Lowers opponent's Attack stat one stage.",
-    speed_priority: 1,
-    reflected_by_magic_coat_and_magic_bounce: true,
+# baby_doll_eyes = Move.create!(
+#     name: 'baby_doll_eyes',
+#     description: 'Baby-Doll Eyes',
+#     type: fairy,
+#     category: other,
+#     power_points: 30,
+#     base_power: 0,
+#     accuracy: 100,
+#     battle_effect: "The user stares at the target with its baby-doll eyes, which lowers its Attack stat. This move always goes first.",
+#     secondary_effect:"Lowers opponent's Attack stat one stage.",
+#     speed_priority: 1,
+#     reflected_by_magic_coat_and_magic_bounce: true,
                  
-)
+# )
 
 # Pokémon ---------------------------------------------------------------------------------------------------
 
