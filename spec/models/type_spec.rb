c3c0894be5f51c0b1d92338d003cde855457312d
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2017-01-20 05:42:49 +0000

    Update type_spec.rb with model validation tests

--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Type, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid atributes" do
+    pokemon_type = build(:type)
+    expect(pokemon_type).to be_valid
+  end
+
+  it "must have unique name" do
+    pokemon_type = create(:type)
+    expect(pokemon_type).to be_valid
+
+    pokemon_type_invalid = build(:type, name: pokemon_type.name)
+    expect(pokemon_type_invalid).to be_invalid
+  end
 end
