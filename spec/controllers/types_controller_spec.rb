937575e810ecb96523930f8d56b485664732e929
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-01-20 04:55:51 -0200

    Revising types controller spec

--- spec/controllers/types_controller_spec.rb
+++ spec/controllers/types_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe TypesController, type: :controller do
     end
 
     it "should return all types when asking JSON format" do
-      FactoryGirl.create_list(:type, 5)
+      create_list(:type, 5)
       get :index, format: :json
       parsed_response = JSON.parse(response.body)
       expect(parsed_response.length).to eq(5)
@@ -21,7 +21,7 @@ RSpec.describe TypesController, type: :controller do
     end
     
     it "should return all types when asking XML format" do
-      FactoryGirl.create_list(:type, 5)
+      create_list(:type, 5)
       get :index, format: :xml
       parsed_response = XmlSimple.xml_in(response.body)
       expect(parsed_response['type'].length).to eq(5)
