30a25d22d7c8859a79f44e52196f484f6f4654f1
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-01-20 04:18:58 -0200

    Adding specs to abilities, adjusting identations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,2 +1,5 @@
 class Ability < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :description, presence: true
+  validates :effect, presence: true
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Ability, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with attributes" do
+    ability = build(:ability)
+    expect(ability).to be_valid
+  end
+
+  it "must have a name" do
+    ability = build(:ability, name: "")
+    expect(ability).to be_invalid
+  end
+
+  it "must have unique name" do
+    ability = create(:ability)
+
+    ability_invalid = build(:ability, name: ability.name)
+    expect(ability_invalid).to be_invalid
+  end
+
+  it "must have a description" do
+    ability = build(:ability, description: "")
+    expect(ability).to be_invalid
+  end
+
+  it "must have an effect" do
+    ability = build(:ability, effect: "")
+    expect(ability).to be_invalid
+  end
 end
