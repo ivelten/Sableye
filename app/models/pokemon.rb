f22e910721b83b3fda87e8a65c307b21d74e1f2a
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2017-01-22 01:06:21 +0000

    Add Pokemon model validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -4,4 +4,22 @@ class Pokemon < ApplicationRecord
   belongs_to :ability_1, class_name: Ability, foreign_key: :ability_1_id
   belongs_to :ability_2, optional: true, class_name: Ability, foreign_key: :ability_1_id
   belongs_to :hidden_ability, class_name: Ability, foreign_key: :hidden_ability_id
+
+  # validations
+  validates :id, presence: true, numericality: { only_integer: true }
+  validates :name, presence: true
+  validates :classification, presence: true
+  validates :height, presence: true, numericality: true
+  validates :weight, presence: true, numericality: true
+  validates :capture_rate, presence: true, numericality: { only_integer: true }
+  validates :base_egg_steps, presence: true, numericality: { only_integer: true }
+  validates :male_gender_ratio, presence: true, numericality: true
+  validates :experience_growth, presence: true, numericality: { only_integer: true }
+  validates :base_happiness, presence: true, numericality: { only_integer: true }
+  validates :hp, presence: true, numericality: { only_integer: true }
+  validates :attack, presence: true, numericality: { only_integer: true }
+  validates :defense, presence: true, numericality: { only_integer: true }
+  validates :sp_attack, presence: true, numericality: { only_integer: true }
+  validates :sp_defense, presence: true, numericality: { only_integer: true }
+  validates :speed, presence: true, numericality: { only_integer: true }
 end
