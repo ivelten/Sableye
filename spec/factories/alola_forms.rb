7bb59058b5dad646ec3f2d988667bfea1f589e89
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2017-01-22 01:51:31 +0000

    Update alola form specs with model validations

--- spec/factories/alola_forms.rb
+++ spec/factories/alola_forms.rb
@@ -1,4 +1,17 @@
 FactoryGirl.define do
   factory :alola_form do
+    association :pokemon, factory: :pokemon
+    association :type_1, factory: :type
+    association :type_2, factory: :type
+    association :ability_1, factory: :ability
+    association :ability_2, factory: :ability
+    association :hidden_ability, factory: :ability
+    height 10.0
+    weight 10.0
+    attack 10
+    defense 10
+    sp_attack 10
+    sp_defense 10
+    speed 3
   end
 end
