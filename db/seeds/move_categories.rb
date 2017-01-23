fac092278682616fbf2dc77917a99df6571aeff8
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2017-01-23 05:07:55 +0000

    Add move categories seed data

---
+++ db/seeds/move_categories.rb
@@ -0,0 +1,14 @@
+MoveCategory.create!(
+  name: 'Physical',
+  description: 'Physical moves cause direct damage based on move base power and pokémon Attack status.'
+)
+
+MoveCategory.create!(
+  name: 'Special',
+  description: 'Special moves cause direct damage based on move base power and pokémon Special Attack status.'
+)
+
+MoveCategory.create!(
+  name: 'Other',
+  description: 'Moves of this category have no direct damage, instead they cause diverse effects in battle.'
+)
