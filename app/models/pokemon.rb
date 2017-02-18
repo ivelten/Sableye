32c7f1ce94c48e985179189d7e65ab4545bc6485
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2017-02-18 14:19:02 -0200

    Fixing rubocop offense

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon < ApplicationRecord
   belongs_to :type_2, optional: true, class_name: Type, foreign_key: :type_2_id
   belongs_to :ability_1, class_name: Ability, foreign_key: :ability_1_id
   belongs_to :ability_2, optional: true, class_name: Ability, foreign_key: :ability_1_id
-  belongs_to :hidden_ability, optional:true, class_name: Ability, foreign_key: :hidden_ability_id
+  belongs_to :hidden_ability, optional: true, class_name: Ability, foreign_key: :hidden_ability_id
 
   validates :id, presence: true, numericality: { only_integer: true }
   validates :name, presence: true
